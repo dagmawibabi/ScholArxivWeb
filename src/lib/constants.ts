f43cd931dfa63f8c99513005bb6e0ebf79b09525
dagmawibabi <1babidagi@gmail.com>
2024-10-05 12:21:55 +0300

    i should sleep

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,6 @@
-export const baseURL = 'http://localhost:6400/arxiv';
-export const authBaseURL = 'http://localhost:6400';
-export const aiBaseURL = 'http://localhost:6400/gemini';
-// export const baseURL = 'https://scholarxivapi.onrender.com/arxiv';
-// export const authBaseURL = 'https://scholarxivapi.onrender.com';
+// export const baseURL = 'http://localhost:6400/arxiv';
+// export const authBaseURL = 'http://localhost:6400';
+// export const aiBaseURL = 'http://localhost:6400/gemini';
+export const baseURL = 'https://scholarxivapi.onrender.com/arxiv';
+export const authBaseURL = 'https://scholarxivapi.onrender.com';
+export const aiBaseURL = 'https://scholarxivapi.onrender.com/gemini';
